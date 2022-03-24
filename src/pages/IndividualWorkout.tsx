b9a57a50df728aed20378b2b8d7c18550176a78b
mihailryzij0 <mihailryzij0@gmail.com>
2022-03-24 21:06:18 +0300

    add select IndividualWorkout

--- src/pages/IndividualWorkout.tsx
+++ src/pages/IndividualWorkout.tsx
@@ -19,12 +19,62 @@ export default function IndividualWorkout() {
            displayEmpty
            inputProps={{ 'aria-label': 'Without label' }}
           >
-                         <MenuItem value="">
+           <MenuItem value="">
             <em>Уровень подготовки</em>
           </MenuItem>
           <MenuItem value={10}>Начинающий</MenuItem>
           <MenuItem value={21}>Средний</MenuItem>
           <MenuItem value={22}>Выше среднего</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <Select
+           sx={{width:'360px'}}
+           value={age}
+           onChange={handleChange}
+           displayEmpty
+           inputProps={{ 'aria-label': 'Without label' }}
+          >
+           <MenuItem value="">
+            <em>Желаемая сложность</em>
+          </MenuItem>
+          <MenuItem value={10}>Тренировки средней тяжести</MenuItem>
+          <MenuItem value={21}>Тренировки полегче</MenuItem>
+          <MenuItem value={22}>Тренировки посложнее</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <Select
+           sx={{width:'360px'}}
+           value={age}
+           onChange={handleChange}
+           displayEmpty
+           inputProps={{ 'aria-label': 'Without label' }}
+          >
+           <MenuItem value="">
+            <em>Цель тренировок</em>
+          </MenuItem>
+          <MenuItem value={10}>Похудеть</MenuItem>
+          <MenuItem value={21}>Набрать массу</MenuItem>
+          <MenuItem value={22}>Повысить силовые</MenuItem>
+          <MenuItem value={22}>Повысить выносливость</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl >
+          <Select
+           sx={{width:'360px'}}
+           value={age}
+           onChange={handleChange}
+           displayEmpty
+           inputProps={{ 'aria-label': 'Without label' }}
+          >
+           <MenuItem value="">
+            <em>Чему уделить внимание</em>
+          </MenuItem>
+          <MenuItem value={10}>Ноги</MenuItem>
+          <MenuItem value={21}>Спина</MenuItem>
+          <MenuItem value={22}>Пресс</MenuItem>
+          <MenuItem value={22}>Грудь</MenuItem>
           </Select>
         </FormControl>
       </Box>
