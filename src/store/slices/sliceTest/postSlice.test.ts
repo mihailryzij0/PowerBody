2a01fd1b0be98477b600161270328ccf9a3e8810
mihailryzij0 <mihailryzij0@gmail.com>
2022-04-19 19:54:22 +0300

    created postSlice.test.ts filу

--- src/store/slices/sliceTest/postSlice.test.ts
+++ src/store/slices/sliceTest/postSlice.test.ts
@@ -1,9 +1,7 @@
 import { store } from "../../store";
-import { getPostCards } from "../cardsSlice";
 import { getPostData } from "../postSlice";
 
-const state = {
-  postData: {
+const receivedData  = {
     authorId: "test123",
     author: "Михаил",
     image: "https://firebasestorage.googleapis.com",
@@ -11,24 +9,23 @@ const state = {
     rating: "5",
     title: "test",
     id: 123,
-  },
 };
 
 jest.mock("../../../firebase", () => ({
   getFirebaseData: jest
     .fn()
-    .mockResolvedValueOnce(state)
+    .mockResolvedValueOnce(receivedData)
     .mockRejectedValueOnce("ошибка"),
 }));
 
-describe("CardSlice", () => {
+describe("postSlice", () => {
   beforeEach(() => {});
 
   it("asyncThunk getPostData fulfilled", async () => {
     await store.dispatch(getPostData(123));
     const { post } = store.getState();
     expect(post.status).toBe("fulfilled");
-    expect(post.postData).toEqual(state.postData);
+    expect(post.postData).toEqual(receivedData);
   });
   it("asyncThunk getPostData rejected", async () => {
     await store.dispatch(getPostData(123));
